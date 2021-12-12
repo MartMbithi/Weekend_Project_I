ef6be0f07b72e74e7cefe9c8d9f7ecb966dbf9f1
Martin Mbithi <martdevelopers254@gmail.com>
2021-12-12 12:19:44 +0300

    Impement Grades Attained

---
+++ overall_perfomance.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ * Created on Sat Dec 11 2021
+ *
+ *  Devlan - devlan.co.ke 
+ *
+ * hello@devlan.info
+ *
+ *
+ * The Devlan End User License Agreement
+ *
+ * Copyright (c) 2021 Devlan
+ *
+ * 1. GRANT OF LICENSE
+ * Devlan hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ * install and activate this system on two separated computers solely for your personal and non-commercial use,
+ * unless you have purchased a commercial license from Devlan. Sharing this Software with other individuals, 
+ * or allowing other individuals to view the contents of this Software, is in violation of this license.
+ * You may not make the Software available on a network, or in any way provide the Software to multiple users
+ * unless you have first purchased at least a multi-user license from Devlan.
+ *
+ * 2. COPYRIGHT 
+ * The Software is owned by Devlan and protected by copyright law and international copyright treaties. 
+ * You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ * 3. RESTRICTIONS ON USE
+ * You may not, and you may not permit others to
+ * (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ * (b) modify, distribute, or create derivative works of the Software;
+ * (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ * otherwise exploit the Software.  
+ *
+ * 4. TERM
+ * This License is effective until terminated. 
+ * You may terminate it at any time by destroying the Software, together with all copies thereof.
+ * This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ * Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ * 5. NO OTHER WARRANTIES. 
+ * Devlan  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ * Devlan SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ * SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ * ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ * THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ * HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ * 6. SEVERABILITY
+ * In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ * affect the validity of the remaining portions of this license.
+ *
+ * 7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL DEVLAN  OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ * CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ * USE OF THE SOFTWARE, EVEN IF DEVLAN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ * IN NO EVENT WILL DEVLAN  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ * TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ */
+session_start();
+require_once('config/config.php');/* Load Config File */
+require_once('config/checklogin.php');/* Load Checklogin */
+checklogin();/* Invoke Check Login, Prevent Watery Log Ins & Session Hijacking */
+require_once('partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('partials/aside.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-bold">Marks</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                                <li class="breadcrumb-item"><a href="">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Overall Perfomance</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div>
+            </div>
+
+            <section class="content">
+                <div class="container-fluid">
+                    <hr>
+                    <div class="row">
+                        <div class="col-12">
+                            <table id="" class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Student Details</th>
+                                        <th>Subject Details </th>
+                                        <th>Aggregate Marks</th>
+                                        <th>Grade Attained</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $ret = "SELECT * FROM marks m
+                                    INNER JOIN student s ON m.marks_student_id = s.student_id
+                                    INNER JOIN subject sb ON m.marks_subject_id = sb.subject_id";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute(); //ok
+                                    $res = $stmt->get_result();
+                                    while ($marks = $res->fetch_object()) {
+                                        /* Compute Grade */
+                                        if ($marks->marks_aggregate >= 70 && $marks->marks_aggregate <= 100) {
+                                            $grade = "A";
+                                        } elseif ($marks->marks_aggregate >= 60 && $marks->marks_aggregate <= 69) {
+                                            $grade = "B";
+                                        } elseif ($marks->marks_aggregate >= 50 && $marks->marks_aggregate <= 59) {
+                                            $grade = "C";
+                                        } elseif ($marks->marks_aggregate >= 40 && $marks->marks_aggregate <= 49) {
+                                            $grade = "D";
+                                        } elseif ($marks->marks_aggregate >= 30 && $marks->marks_aggregate <= 39) {
+                                            $grade = "E";
+                                        } else {
+                                            $grade = "F";
+                                        }
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $marks->student_admno . ' ' . $marks->student_name; ?></td>
+                                            <td>
+                                                <?php echo $marks->subject_code . ' ' . $marks->subject_name; ?></td>
+                                            </td>
+                                            <td>
+                                                <?php echo $marks->marks_aggregate; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $grade; ?>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- Main Footer -->
+        <?php require_once('partials/footer.php'); ?>
+    </div>
+    <?php require_once('partials/scripts.php'); ?>
+</body>
+
+</html>
